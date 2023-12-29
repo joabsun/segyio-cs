ff88440a5f670fb469a7795d8ddc0c305536578d
joabsun <32539546+joabsun@users.noreply.github.com>
2023-12-29 22:56:16 +0800

    Update ByteOrder.cs

--- SegyIO/Model/ByteOrder.cs
+++ SegyIO/Model/ByteOrder.cs
@@ -1,1 +1,18 @@
+namespace SegyIO.Model
+{
+    /// <summary>
+    /// Byte Order
+    /// </summary>
+    public enum ByteOrder
+    {
+        /// <summary>
+        /// Big Endian
+        /// </summary>
+        BigEndian = 0,
 
+        /// <summary>
+        /// Little Endian
+        /// </summary>
+        LittleEndian = 1
+    }
+}
