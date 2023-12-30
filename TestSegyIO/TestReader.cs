991cb830b84bee1f560ee4b49805ecd5ef5b70ee
joabsun <32539546+joabsun@users.noreply.github.com>
2023-12-30 08:19:18 +0800

    Add files via upload

---
+++ TestSegyIO/TestReader.cs
@@ -0,0 +1,196 @@
+﻿using System.Diagnostics;
+using SegyIO.IO;
+using SegyIO.Model;
+
+namespace TestSegyIO
+{
+    internal class TestReader
+    {
+        /// <summary>
+        /// Read some information in the SEGY file, including: number of traces, number of sampling points per trace,
+        /// sampling rate, starting time
+        /// </summary>
+        public static void ReadSegyFileInfo()
+        {
+            string folder = Path.GetDirectoryName(Process.GetCurrentProcess().MainModule?.FileName);
+            string segyFilename = Path.Combine(folder, "test1.segy");
+
+            ByteOrder byteOrder = SegyReader.GetByteOrder(segyFilename);
+
+            FileStream fs = new FileStream(segyFilename, FileMode.Open, FileAccess.Read);
+            BinaryReader br = new BinaryReader(fs);
+
+            SegyReader.ReadTextHeader(br); //读文本头
+            SegyReelHeader reelHeader = SegyReader.ReadReelHeader(br, byteOrder);
+
+            var DataFormatCode = reelHeader.DataFormatCode;
+            int samplesPerTrace = reelHeader.SmpPerTrc;
+            int sampleInt = reelHeader.SmpInt;
+            var bytesPerSample = SegyReader.GetBytesPerSample(DataFormatCode);
+            var traceNumber = (int)((br.BaseStream.Length - 3600) / (240 + samplesPerTrace * bytesPerSample));
+            SegyTraceHeader Traceheader = SegyReader.ReadTraceHeader(br, byteOrder);
+            int delayTime = Traceheader.DelayRec;
+
+            br.Close();
+
+            string s = "Trace number: " + traceNumber + Environment.NewLine;
+            s += "Sample number per trace: " + samplesPerTrace + Environment.NewLine;
+            s += "Sample rate(us): " + sampleInt + Environment.NewLine;
+            s += "Delay time: " + delayTime;
+
+            Console.WriteLine(s);
+        }
+
+        /// <summary>
+        /// 读取segy文件中的卷头信息(包括文本和二进制卷头)
+        /// </summary>
+        /// <param name="segyFile"></param>
+        /// <returns>列表的第一对象为文本卷头,第二个对象为二进制卷头,第三个对象为字节序</returns>
+        public static string ReadTextHeader(string segyFile)
+        {
+            FileStream fs = new FileStream(segyFile, FileMode.Open, FileAccess.Read);
+            BinaryReader br = new BinaryReader(fs);
+
+            //读文本卷头3200字节
+            string EBCDICHeader = SegyReader.ReadTextHeader(br);
+
+            br.Close();
+
+            return EBCDICHeader;
+        }
+
+        /// <summary>
+        /// 读取segy文件中的二进制卷头信息
+        /// </summary>
+        /// <param name="segyfile"></param>
+        /// <returns>二进制卷头</returns>
+        public static SegyReelHeader ReadSegyReelHeader(string segyfile)
+        {
+            ByteOrder byteOrder = SegyReader.GetByteOrder(segyfile);
+
+            FileStream fs = new FileStream(segyfile, FileMode.Open, FileAccess.Read);
+            BinaryReader br = new BinaryReader(fs);
+
+            //跳过文本卷头3200字节
+            br.BaseStream.Seek(3200, SeekOrigin.Begin);
+            SegyReelHeader Reelheader = SegyReader.ReadReelHeader(br, byteOrder);
+
+            br.Close();
+
+            return Reelheader;
+        }
+
+        /// <summary>
+        /// 读取道数据, 大文件不建议这种方式读取，可能导致内存溢出。2G内问题不大
+        /// </summary>
+        /// <param name="segyfile">源文件</param>
+        /// <param name="textHdr">返回的文本卷头</param>
+        /// <param name="reelHdr">返回的卷头</param>
+        /// <param name="trcHdrs">返回的道头数组</param>
+        /// <returns>道数据,形如Trc[smp,trc]</returns>
+        public static float[,] ReadSegy(string segyfile, out string textHdr, out SegyReelHeader reelHdr, out SegyTraceHeader[] trcHdrs)
+        {
+            ByteOrder byteOrder = SegyReader.GetByteOrder(segyfile);
+
+            FileStream fs = new FileStream(segyfile, FileMode.Open, FileAccess.Read);
+            BinaryReader br = new BinaryReader(fs);
+
+            textHdr = SegyReader.ReadTextHeader(br);
+            reelHdr = SegyReader.ReadReelHeader(br, byteOrder);
+
+            var dataFormat = reelHdr.DataFormatCode;
+            int smpPerTrc = reelHdr.SmpPerTrc;
+            var bytesPerSmp = SegyReader.GetBytesPerSample(dataFormat);  //表示每样点字节数,用于计算每道字节数
+            var bytesPerTrc = 240 + smpPerTrc * bytesPerSmp;  //每道字节数
+
+            var trcs = (int)((br.BaseStream.Length - 3600) / bytesPerTrc);
+
+            trcHdrs = new SegyTraceHeader[trcs];
+            float[,] trcData = new float[smpPerTrc, trcs];
+            for (int i = 0; i < trcs; i++)
+            {
+                long skip = 3600 + bytesPerTrc * i; //跳过的字节数
+                br.BaseStream.Seek(skip, SeekOrigin.Begin);
+
+                trcHdrs[i] = SegyReader.ReadTraceHeader(br, byteOrder);
+                float[] trc = SegyReader.ReadTrcData(br, smpPerTrc, bytesPerSmp, byteOrder, dataFormat);
+                for (int j = 0; j < smpPerTrc; j++)
+                {
+                    trcData[j, i] = trc[j];
+                }
+            }
+
+            br.Close();
+            return trcData;
+        }
+
+        /// <summary>
+        /// 读取无卷头的SEGY道数据,形如Trc[smp,trc]
+        /// </summary>
+        /// <param name="segyfile">源文件</param>
+        /// <param name="trcHdrs">返回的道头数组</param>
+        /// <param name="byteOrder"></param>
+        /// <param name="dataFormat"></param>
+        /// <returns>道数据,形如Trc[smp,trc]</returns>
+        public static float[,] ReadSegyNoReel(string segyfile, ByteOrder byteOrder, ushort dataFormat, out SegyTraceHeader[] trcHdrs)
+        {
+            FileStream fs = new FileStream(segyfile, FileMode.Open, FileAccess.Read);
+            BinaryReader br = new BinaryReader(fs);
+
+            SegyTraceHeader trcHdr = SegyReader.ReadTraceHeader(br, byteOrder);
+
+            int smpPerTrc = trcHdr.SamplesThisTrace;
+            var bytesPerSmp = SegyReader.GetBytesPerSample(dataFormat);  //表示每样点字节数,用于计算每道字节数
+            var bytesPerTrc = 240 + smpPerTrc * bytesPerSmp;  //每道字节数
+
+            var trcs = (int)(br.BaseStream.Length / bytesPerTrc);
+
+            trcHdrs = new SegyTraceHeader[trcs];
+            float[,] trcData = new float[smpPerTrc, trcs];
+            br.BaseStream.Seek(0, SeekOrigin.Begin); //重置读取位置为开始
+            for (int i = 0; i < trcs; i++)
+            {
+                trcHdrs[i] = SegyReader.ReadTraceHeader(br, byteOrder);
+                float[] trc = SegyReader.ReadTrcData(br, smpPerTrc, bytesPerSmp, byteOrder, dataFormat);
+                for (int j = 0; j < smpPerTrc; j++)
+                {
+                    trcData[j, i] = trc[j];
+                }
+            }
+
+            br.Close();
+            return trcData;
+        }
+
+        /// <summary>
+        /// 读取SEGY单道数据
+        /// </summary>
+        /// <param name="segyfile">源文件</param>
+        /// <param name="trcNo">道序号</param>
+        /// <returns>道数据</returns>
+        public static float[] ReadATrcData(string segyfile, int trcNo)
+        {
+            ByteOrder byteOrder = SegyReader.GetByteOrder(segyfile);
+
+            FileStream fs = new FileStream(segyfile, FileMode.Open, FileAccess.Read);
+            BinaryReader br = new BinaryReader(fs);
+
+            SegyReader.ReadTextHeader(br);
+            SegyReelHeader reelHdr = SegyReader.ReadReelHeader(br, byteOrder);
+
+            var dataFormat = reelHdr.DataFormatCode;
+            int smpPerTrc = reelHdr.SmpPerTrc;
+            var bytesPerSmp = SegyReader.GetBytesPerSample(dataFormat); //表示每样点字节数,用于计算每道字节数
+            var bytesPerTrc = 240 + smpPerTrc * bytesPerSmp; //每道字节数
+
+            var skipBytes = 3600 + bytesPerTrc * trcNo; //读取当前道要跳过的字节数,从文件开始计算
+            br.BaseStream.Seek(skipBytes, SeekOrigin.Begin);
+
+            float[] trc = SegyReader.ReadTrcData(br, smpPerTrc, bytesPerSmp, byteOrder, dataFormat);
+
+            br.Close();
+            return trc;
+        }
+
+    }
+}
---
+++ TestSegyIO/TestWriter.cs
@@ -0,0 +1,134 @@
+﻿using SegyIO.IO;
+using SegyIO.Model;
+
+namespace TestSegyIO
+{
+    internal class TestWriter
+    {
+        /// <summary>
+        /// 写SEGY数据
+        /// </summary>
+        /// <param name="outfile">输出文件</param>
+        /// <param name="txtHdr">文本卷头</param>
+        /// <param name="reelHdr">二进制卷头</param>
+        /// <param name="trcHdrs">道头数组</param>
+        /// <param name="trcData">道数据,形如Trc[smp,trc]</param>
+        /// <param name="byteOrder">字节序</param>
+        /// <param name="dataFormat">数据格式码</param>
+        public static void WriteSegy(string outfile, string txtHdr, SegyReelHeader reelHdr, SegyTraceHeader[] trcHdrs,
+            float[,] trcData, ByteOrder byteOrder, ushort dataFormat)
+        {
+            FileStream fs = new FileStream(outfile, FileMode.Create, FileAccess.Write);
+            BinaryWriter bw = new BinaryWriter(fs);
+
+            SegyWriter.WriteTextHeader(bw, txtHdr);
+            SegyWriter.WriteReelHeader(bw, reelHdr, byteOrder);
+
+            var trcCount = trcData.GetLength(1);
+            var smpCount = trcData.GetLength(0);
+
+            for (int i = 0; i < trcCount; i++)
+            {
+                SegyWriter.WriteTraceHeader(bw, trcHdrs[i], byteOrder);
+
+                float[] ampls = new float[smpCount];
+                for (int j = 0; j < smpCount; j++)
+                {
+                    ampls[j] = trcData[j, i];
+                }
+                SegyWriter.WriteTrcData(bw, ampls, byteOrder, dataFormat);
+            }
+
+            bw.Close();
+        }
+
+        /// <summary>
+        /// 根据抽取的道序号列表从源文件抽取道数据存储于新文件
+        /// </summary>
+        /// <param name="srcSegy">源文件</param>
+        /// <param name="outSegy">新文件</param>
+        /// <param name="trcNos">要抽取的道序号列表</param>
+        /// <param name="minTime">ms</param>
+        /// <param name="maxTime">ms </param>
+        /// <param name="stepTime">ms</param>
+        public static void ExtractSegy(string srcSegy, string outSegy, IList<int> trcNos, float minTime, float maxTime, float stepTime)
+        {
+            ByteOrder byteOrder = SegyReader.GetByteOrder(srcSegy);
+
+            FileStream fs = new FileStream(srcSegy, FileMode.Open, FileAccess.Read);
+            BinaryReader br = new BinaryReader(fs);
+            FileStream fs1 = new FileStream(outSegy, FileMode.Create, FileAccess.Write);
+            BinaryWriter bw = new BinaryWriter(fs1);
+
+            //读卷头
+            string EBCDICHeader = SegyReader.ReadTextHeader(br);
+            SegyReelHeader reelheader = SegyReader.ReadReelHeader(br, byteOrder);
+
+            var dataFormat = reelheader.DataFormatCode;
+            int srcSmpInt = reelheader.SmpInt;
+            int srcSmpPerTrc = reelheader.SmpPerTrc;
+            var bytesPerSmp = SegyReader.GetBytesPerSample(dataFormat);  //表示每样点字节数,用于计算每道字节数
+            var bytesPerTrcData = srcSmpPerTrc * bytesPerSmp;  //每道数据字节数
+            var bytesPerTrc = 240 + bytesPerTrcData;  //每道字节数
+
+            var trcs = trcNos.Count;
+
+            //读写道头及数据
+            SegyTraceHeader trcHdr1 = SegyReader.ReadTraceHeader(br, byteOrder); //源数据第一道道头
+            int srcMinTime = trcHdr1.DelayRec;
+            var outSmpPerTrc = (int)((maxTime - minTime) / stepTime) + 1; //输出数据的每道样点数
+            var outBytesPerTrc = outSmpPerTrc * bytesPerSmp; //输出数据的每道字节数
+            var outMinSmpNo = (int)((minTime - srcMinTime) * 1000 / srcSmpInt);//输出数据的起始样点数
+            var outMaxSmpNo = (int)((maxTime - srcMinTime) * 1000 / srcSmpInt);//输出数据的结束样点数
+            var skipSmpNo = (int)(stepTime * 1000 / srcSmpInt) - 1;
+
+            //写卷头
+            SegyWriter.WriteTextHeader(bw, EBCDICHeader);
+            reelheader.SmpInt = (ushort)(stepTime * 1000);
+            reelheader.SmpPerTrc = (ushort)outSmpPerTrc;
+            SegyWriter.WriteReelHeader(bw, reelheader, byteOrder);
+            for (int i = 0; i < trcs; i++)
+            {
+                long trcNo = trcNos[i];
+
+                var skipBytes = 3600 + bytesPerTrc * trcNo; //读取当前道要跳过的字节数,从文件开始计算
+                br.BaseStream.Seek(skipBytes, SeekOrigin.Begin);
+
+                SegyTraceHeader trcHdr = SegyReader.ReadTraceHeader(br, byteOrder);
+                trcHdr.DelayRec = (short)minTime;
+                trcHdr.SmplIntvl = reelheader.SmpInt;
+                trcHdr.SamplesThisTrace = reelheader.SmpPerTrc;
+
+                if (skipSmpNo == 0) //未改变输出的采样间隔
+                {
+                    var skipBytesInTrc = bytesPerSmp * outMinSmpNo;
+                    br.BaseStream.Seek(skipBytesInTrc, SeekOrigin.Current);
+                    byte[] trcData = br.ReadBytes(outBytesPerTrc);
+
+                    SegyWriter.WriteTraceHeader(bw, trcHdr, byteOrder);
+                    bw.Write(trcData);
+                }
+                else
+                {
+                    var skipBytesInTrc = bytesPerSmp * outMinSmpNo;
+                    br.BaseStream.Seek(skipBytesInTrc, SeekOrigin.Current);
+                    List<byte> trcData = new List<byte>();
+                    var inc = skipSmpNo + 1;//输出样点的增量
+                    var skipBytes11 = skipSmpNo * bytesPerSmp;//每两个样点间读取时要跳过的字节数
+                    for (int j = outMinSmpNo; j <= outMaxSmpNo; j += inc)
+                    {
+                        byte[] bytesSmp = br.ReadBytes(bytesPerSmp);
+                        trcData.AddRange(bytesSmp);
+                        br.BaseStream.Seek(skipBytes11, SeekOrigin.Current);
+                    }
+                    SegyWriter.WriteTraceHeader(bw, trcHdr, byteOrder);
+                    bw.Write(trcData.ToArray());
+                }
+            }
+
+            br.Close();
+            bw.Close();
+        }
+
+    }
+}
